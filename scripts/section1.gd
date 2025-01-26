9987c11597ce6d94c2941cd6c8761c323ca8e503
Birdboys <colbyjbird@comcast.net>
2025-01-25 23:23:32 -0700

    added parallax and better camera follow

--- scripts/section1.gd
+++ scripts/section1.gd
@@ -5,26 +5,30 @@ extends Node2D
 @onready var cam := $playerCam
 @onready var waterPlayer := $waterPlayer
 @onready var timerLabel := $uiLayer/uiMargin/timerLabel
+@onready var camTrigger := $camTrigger
 
-var cam_move_speed := 150.0
+var cam_move_speed := 250.0
 var game_time := 0.0
 var game_started := false
+var track_cam := false
 var prev_mouse_pos : Vector2
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
 	puffer.do_puff.connect(handlePuff)
+	camTrigger.body_exited.connect(startCamTracking)
 	get_tree().create_timer(2.0).timeout.connect(startGame)
 	prev_mouse_pos = get_global_mouse_position()
 	
 func _process(delta: float) -> void:
-	cam.position = cam.position.move_toward(bubble.position, cam_move_speed)
 	puffer.handleRotation(bubble.position)
 	if game_started:
 		game_time += delta
 		timerLabel.text = str(int(game_time))
 		handleWaterPlayer()
-	
+	if track_cam:
+		cam.position = cam.position.move_toward(bubble.position, cam_move_speed*delta)
+		
 func handlePuff():
 	var push_dir = -(bubble.position.direction_to(puffer.position).normalized())
 	var push_dist = bubble.position.distance_to(puffer.position)
@@ -35,11 +39,14 @@ func startGame():
 	bubble.is_playing = true
 	game_started = true
 	waterPlayer.play(0.0)
+
+func startCamTracking(_bubble):
+	print("START TRACKING")
+	track_cam = true
 	
 func handleWaterPlayer():
 	var current_pos = get_local_mouse_position()
 	var pos_diff = prev_mouse_pos.distance_to(current_pos)
 	pos_diff = clamp(pos_diff, 0.0, 200.0)/200.0
 	prev_mouse_pos = current_pos
-	print(pos_diff)
 	waterPlayer.volume_db = linear_to_db(pos_diff)
