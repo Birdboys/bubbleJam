948b8c842ddf0962cd168b10bec2cec0c62e5c53
Birdboys <colbyjbird@comcast.net>
2025-01-25 22:42:48 -0700

    added woosh sound when moving fish

--- scripts/section1.gd
+++ scripts/section1.gd
@@ -3,17 +3,19 @@ extends Node2D
 @onready var puffer := $pufferFish
 @onready var bubble := $bubble
 @onready var cam := $playerCam
+@onready var waterPlayer := $waterPlayer
 @onready var timerLabel := $uiLayer/uiMargin/timerLabel
 
 var cam_move_speed := 150.0
 var game_time := 0.0
 var game_started := false
+var prev_mouse_pos : Vector2
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
 	puffer.do_puff.connect(handlePuff)
 	get_tree().create_timer(2.0).timeout.connect(startGame)
-	
+	prev_mouse_pos = get_global_mouse_position()
 	
 func _process(delta: float) -> void:
 	cam.position = cam.position.move_toward(bubble.position, cam_move_speed)
@@ -21,6 +23,7 @@ func _process(delta: float) -> void:
 	if game_started:
 		game_time += delta
 		timerLabel.text = str(int(game_time))
+		handleWaterPlayer()
 	
 func handlePuff():
 	var push_dir = -(bubble.position.direction_to(puffer.position).normalized())
@@ -31,3 +34,12 @@ func startGame():
 	puffer.pufferEmpty()
 	bubble.is_playing = true
 	game_started = true
+	waterPlayer.play(0.0)
+	
+func handleWaterPlayer():
+	var current_pos = get_local_mouse_position()
+	var pos_diff = prev_mouse_pos.distance_to(current_pos)
+	pos_diff = clamp(pos_diff, 0.0, 200.0)/200.0
+	prev_mouse_pos = current_pos
+	print(pos_diff)
+	waterPlayer.volume_db = linear_to_db(pos_diff)
